1bdb1ee9a2636b482cf3d676dc4fca10eda61210
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-16 20:10:17 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,7 +28,7 @@ end
 
 def adding
   shipping_manifest = {
-              "whale bone corsets" => 5, 
+  "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
