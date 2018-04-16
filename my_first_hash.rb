149844bd40adb5bbffda9a67f7292abbeef8a2ee
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-16 20:12:58 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,9 +30,9 @@ def adding
   shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2,  "oil paintings" => 3 }
  
 
-shipping_manifest["muskets"]=2
-shipping_manifest["gun powder"]=4
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
-
+puts shipping_manifest
 
 end
